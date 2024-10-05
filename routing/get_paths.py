3614fe03c8e594c61ebee669190c8a74778f1253
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2024-10-05 21:26:30 +1000

    Fix paths to better follow roads

--- routing/get_paths.py
+++ routing/get_paths.py
@@ -8,7 +8,62 @@ from routing.path_costs import get_hours_taken_between_points
 def get_streets_from_location_name(location_name: str) -> list[str]:
     words = location_name.split(" ")
 
-    return [word for word in words if "_" in word]
+    streets = [word for word in words if "_" in word]
+
+    return [streets[0]]
+
+
+def get_opposite_direction(direction: str) -> str:
+    if direction == "N":
+        return "S"
+    elif direction == "S":
+        return "N"
+    elif direction == "E":
+        return "W"
+    elif direction == "W":
+        return "E"
+    elif direction == "NE":
+        return "SW"
+    elif direction == "NW":
+        return "SE"
+    elif direction == "SE":
+        return "NW"
+    elif direction == "SW":
+        return "NE"
+    else:
+        return None
+
+
+def get_neighbours_from_scat(location: dict, all_locations: list[dict]) -> list[int]:
+
+    other_locations = all_locations.copy()
+
+    other_locations.remove(location)
+
+    neighbours = []
+
+    for other_location in other_locations:
+        if other_location["site_number"] == location["site_number"]:
+
+            neighbours.append(other_location["location_id"])
+
+    return neighbours
+
+
+def get_direction_from_location_name(location_name: str) -> str:
+    cardinal_directions = ["N", "S", "E", "W"]
+
+    inter_cardinal_directions = ["NE", "NW", "SE", "SW"]
+
+    all_directions = cardinal_directions + inter_cardinal_directions
+
+    words = location_name.split(" ")
+
+    for word in words:
+        if word in all_directions:
+            return word
+
+    return None
 
 
 def get_neighbours_of_location(location: dict, all_locations: list[dict]) -> list[int]:
@@ -17,18 +72,32 @@ def get_neighbours_of_location(location: dict, all_locations: list[dict]) -> lis
     name = location["name"]
 
     neighbouring_streets = get_streets_from_location_name(name)
-
     neighbours = []
 
     other_locations = all_locations.copy()
 
     other_locations.remove(location)
 
+    direction = get_direction_from_location_name(name)
+
     for other_location in other_locations:
         streets = get_streets_from_location_name(other_location["name"])
 
-        if any(street in streets for street in neighbouring_streets):
-            neighbours.append(other_location["location_id"])
+        for street in streets:
+
+            if street not in neighbouring_streets:
+                continue
+
+            other_direction = get_direction_from_location_name(other_location["name"])
+
+            if (
+                other_direction == get_opposite_direction(direction)
+                or other_direction == direction
+            ):
+                neighbours.append(other_location["location_id"])
+
+    # add some neighbours from same scat in case they don't have the same street name
+    neighbours = set(neighbours + get_neighbours_from_scat(location, all_locations))
 
     return neighbours
 
