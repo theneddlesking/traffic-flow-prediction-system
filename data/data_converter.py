7b14cfea4fbda3f5bfd51a65c989936282b91845
Anton Chuchuva <anton@chuchuva.com>
2024-10-08 21:01:02 +1100

    Update data_converter.py

--- data/data_converter.py
+++ data/data_converter.py
@@ -28,24 +28,28 @@ def create_train_test_split_from_df(df, train_test_proportion, target, lags):
     df_train, df_test = df[:int(len(df) * train_test_proportion)], df[int(len(df) * train_test_proportion):]
 
     scaler = MinMaxScaler(feature_range=(0, 1)).fit(df[target].values.reshape(-1, 1))
-    normalised_flow = scaler.transform(df_train[target].values.reshape(-1, 1)).reshape(1, -1)[0]
-    normalised_flow = scaler.transform(df_test[target].values.reshape(-1, 1)).reshape(1, -1)[0]
+    train_normalised_flow = scaler.transform(df_train[target].values.reshape(-1, 1)).reshape(1, -1)[0]
+    test_normalised_flow = scaler.transform(df_test[target].values.reshape(-1, 1)).reshape(1, -1)[0]
 
     train, test = [], []
 
     for i in range(lags, df_train):
-        train.append(normalised_flow[i - lags: i + 1])
+        train.append(train_normalised_flow[i - lags: i + 1])
 
     for i in range(lags, df_test):
-        test.append(normalised_flow[i - lags: i + 1])
+        test.append(test_normalised_flow[i - lags: i + 1])
 
     train = np.array(train)
     test = np.array(test)
     np.random.shuffle(train)
 
-    X_train = train[:, :-1]
+    x_train = train[:, :-1]
     y_train = train[:, -1]
-    X_test = test[:, :-1]
+    x_test = test[:, :-1]
     y_test = test[:, -1]
+    y_test_original = scaler.inverse_transform(y_test.reshape(-1, 1)).reshape(1, -1)[0]
+    
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-    return df[target].values
+    return x_train, y_train, x_test, y_test, y_test_original
