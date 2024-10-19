2a9282f304e2c020f5f0213d6229d856a89de931
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2024-10-19 20:23:34 +1100

    Generate multiple routes

--- routing/a_star_router.py
+++ routing/a_star_router.py
@@ -4,10 +4,15 @@ from routing.point import RoutingPoint
 from routing.road_network import RoadNetwork
 from routing.route import Route
 from routing.router import Router
+from routing.time_estimator import TimeEstimator
+from routing.time_graph import TimeGraph
 
 
 class AStarRouter(Router):
     """A router that uses the A* algorithm to find the shortest path between two RoutingPoints."""
+
+    def __init__(self, time_estimator: TimeEstimator):
+        self.time_estimator = time_estimator
 
     async def find_route(
         self,
@@ -18,7 +23,9 @@ class AStarRouter(Router):
     ) -> Route:
         """Find the shortest route between two RoutingPoints using the A* algorithm."""
 
-        time_graph = await self.create_graph(time_of_day, network)
+        time_graph = await TimeGraph(
+            network, self.time_estimator, time_of_day
+        ).initialise()
 
         return self.find_route_from_time_graph(start, end, time_graph, network)
 
@@ -26,13 +33,16 @@ class AStarRouter(Router):
         self,
         start: RoutingPoint,
         end: RoutingPoint,
-        time_graph: dict[int, dict[int, int]],
+        time_graph: TimeGraph,
         network: RoadNetwork,
     ) -> Route:
         """Find the shortest route between two RoutingPoints using the A* algorithm."""
 
         path = a_star(
-            time_graph, start.location_id, end.location_id, Heuristics.no_heuristic
+            time_graph.point_graph,
+            start.location_id,
+            end.location_id,
+            Heuristics.no_heuristic,
         )
 
         if path is None:
@@ -55,7 +65,9 @@ class AStarRouter(Router):
     ) -> list[Route]:
         """Find the best routes between two points using Penalty A* algorithm."""
 
-        time_graph = await self.create_graph(time_of_day, network)
+        time_graph = await TimeGraph(
+            network, self.time_estimator, time_of_day
+        ).initialise()
 
         best_routes = []
 
@@ -66,6 +78,80 @@ class AStarRouter(Router):
 
         best_routes.append(initial_route)
 
-        # TODO implement penalty A* algorithm
+        def break_condition(
+            found_routes: list[Route],
+            route: Route,
+            number_of_routes: int,
+            search_attempts: int,
+        ) -> bool:
+            """Check when to stop the search for best routes."""
+            # TODO implement break_condition that considers the route itself
+            return number_of_routes >= 3 or search_attempts >= 10
 
-        return best_routes
+        search_attempts = 0
+
+        # NOTE: this is an arbitrary penalty, could we tweak this or make it dynamic?
+        penalty_hours = 1 / 60
+
+        while not break_condition(
+            best_routes, best_routes[-1], len(best_routes), search_attempts
+        ):
+            search_attempts += 1
+
+            # penalise the previous route
+            time_graph = self.penalise_route(best_routes[-1], penalty_hours, time_graph)
+
+            # find the next best route
+            next_best_route = self.find_route_from_time_graph(
+                start, end, time_graph, network
+            )
+
+            # already found the same route or no more routes to find
+            if next_best_route is None or next_best_route in best_routes:
+                break
+
+            best_routes.append(next_best_route)
+
+        # get the actual time taken, not the penalised time
+
+        # TODO this could be simplified somehow, but it should be pretty fast
+        # because there won't be many points in the phony network
+
+        actual_routes = []
+
+        for route in best_routes:
+
+            # phony network that only contains the waypoints of the route
+            phony_network = RoadNetwork(route.waypoints)
+
+            new_time_graph = await TimeGraph(
+                phony_network, self.time_estimator, time_of_day
+            ).initialise()
+
+            # find the actual time taken
+            actual_route = self.find_route_from_time_graph(
+                start, end, new_time_graph, phony_network
+            )
+
+            actual_routes.append(actual_route)
+
+        return actual_routes
+
+    def penalise_route(
+        self, route: Route, penalty_hours: int, time_graph: TimeGraph
+    ) -> TimeGraph:
+        """Penalise a route on the graph by adding a penalty to the edges."""
+
+        # edges to penalise
+        for waypoint in route.waypoints:
+            # get the neighbours of the waypoint
+            neighbours = time_graph.point_graph[waypoint.location_id]
+
+            for neighbour_id, _ in neighbours.items():
+
+                # penalise the edge
+                time_graph.point_graph[waypoint.location_id][
+                    neighbour_id
+                ] += penalty_hours
+
+        return time_graph
---
+++ routing/time_graph.py
@@ -0,0 +1,54 @@
+from routing.road_network import RoadNetwork
+from routing.time_estimator import TimeEstimator
+
+
+class TimeGraph:
+    """A representation of a graph with time taken to travel between points"""
+
+    def __init__(
+        self, road_network: RoadNetwork, time_estimator: TimeEstimator, time_of_day: str
+    ):
+        self.road_network = road_network
+        self.time_estimator = time_estimator
+        self.time_of_day = time_of_day
+
+        self.point_graph = None
+
+    async def initialise(self) -> "TimeGraph":
+        """Async initialisation of the TimeGraph"""
+
+        self.point_graph = await self.create_point_graph(
+            self.time_of_day, self.road_network
+        )
+
+        return self
+
+    async def create_point_graph(
+        self,
+        time_of_day: int,
+        road_network: RoadNetwork,
+    ) -> dict[int, dict[int, int]]:
+        """Create a graph of the road network with time taken to travel between points."""
+
+        point_graph = {}
+
+        point_graph = road_network.network
+
+        time_graph = {}
+
+        for point, neighbours in point_graph.items():
+
+            key = point.location_id
+
+            time_graph[key] = {}
+
+            for neighbour in neighbours:
+                hours_taken = (
+                    await self.time_estimator.estimate_hours_taken_between_points(
+                        point, neighbour, time_of_day
+                    )
+                )
+
+                time_graph[key][neighbour.location_id] = hours_taken
+
+        return time_graph
