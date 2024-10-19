93916e5c3629da7e38e6e1c51150880898a88825
Josh Celestino <114170790+nissanSilviaa@users.noreply.github.com>
2024-10-20 02:49:08 +1100

    Update test.py

--- Testing-Evaluation/test.py
+++ Testing-Evaluation/test.py
@@ -1,4 +1,23 @@
 import pandas as pd
 import requests
-import mysql.connector
+import sqlite3
 
+db_path = "../db/site.db"
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+def get_location_id(lat, long):
+
+    return
+
+def get_time_from_model(start_location_id, end_location_id, time_of_day):
+
+    return None
+
+def compare_time_taken():
+    
+    return
+
+
+input_file = '../true_data/cleanTrueData.csv'
