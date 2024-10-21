d1c6c6c01332d6106744cf1e373e99bf06229502
Josh Celestino <joshisaiah999@gmail.com>
2024-10-21 15:24:23 +1100

    Update test.py

--- Testing-Evaluation/test.py
+++ Testing-Evaluation/test.py
@@ -7,6 +7,18 @@ db_path = "../db/site.db"
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+def test_api_call(start_location_id, end_location_id, time_of_day):
+    url = f"http://localhost:8000/routing/route?start_location_id={start_location_id}&end_location_id={end_location_id}&time_of_day={time_of_day}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        print("API Response:", response.json())
+    else:
+        print(f"API call failed with status code: {response.status_code}")
+
+test_api_call(43, 71, '12:00')
+
+
 def get_location_id(lat, long):
     cursor.execute("SELECT location_id FROM locations WHERE latitude = ? AND longitude = ?", (lat, long))
     result = cursor.fetchone()
@@ -17,7 +29,7 @@ def get_time_from_model(start_location_id, end_location_id, time_of_day):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        return data.get("hours_taken")  # Adjust based on the actual API response format
+        return data.get("hours_taken")  #
     return None
 
 def compare_time_taken():
