35c566db896347419015ef1e1c68a49fd5fee0a1
Josh Celestino <114170790+nissanSilviaa@users.noreply.github.com>
2024-10-22 23:56:18 +1100

    Update test.py

--- Testing-Evaluation/test.py
+++ Testing-Evaluation/test.py
@@ -7,6 +7,7 @@ db_path = "../db/site.db"
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+#testing
 def test_api_call(start_location_id, end_location_id, time_of_day):
     url = f"http://localhost:8000/routing/route?start_location_id={start_location_id}&end_location_id={end_location_id}&time_of_day={time_of_day}"
     response = requests.get(url)
@@ -16,7 +17,6 @@ def test_api_call(start_location_id, end_location_id, time_of_day):
     else:
         print(f"API call failed with status code: {response.status_code}")
 
-test_api_call(43, 71, '12:00')
 
 
 def get_location_id(lat, long):
@@ -45,9 +45,19 @@ def calculate_accuracy(predicted_time, actual_time):
 def compare_time_taken(input_file):
     df = pd.read_csv(input_file)
 
+    results = []
+
     # Iterate over each row in the truedataCSV and compare them against basic_predictions
+
+    for index, row in df.iterrows():
+        start_lat = row['START_LAT']
+        start_long = row['START_LONG']
+        end_lat = row['END_LAT']
+        end_long = row['END_LONG']
+        actual_time = row['Time_Taken']
+        time_of_day = row['Time']
     return
 
-test_api_call(43, 71, '12:00')
+test_api_call(43, 71, '12:00') #testing
 
 input_file = '../true_data/cleanTrueData.csv'
