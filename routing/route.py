9391a95e260188d0c3060efccc750f92e52f98a3
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2024-10-24 10:21:11 +1100

    Fix overlapping routes

--- routing/route.py
+++ routing/route.py
@@ -1,4 +1,5 @@
 from routing.point import RoutingPoint
+from routing.route_direction import RouteDirection
 
 
 class Route:
@@ -8,11 +9,17 @@ class Route:
         self.waypoints = waypoints
         self.hours_taken = hours_taken
 
+        self.directions: list[RouteDirection] = []
+
+        for i in range(len(waypoints) - 1):
+            self.directions.append(RouteDirection(waypoints[i], waypoints[i + 1]))
+
     def as_json(self) -> dict:
         """Convert the Route to a JSON serializable dictionary."""
         return {
             "waypoints": [point.as_json() for point in self.waypoints],
             "hours_taken": self.hours_taken,
+            "directions": [direction.as_json() for direction in self.directions],
         }
 
     def __eq__(self, other: object) -> bool:
@@ -30,4 +37,21 @@ class Route:
         for point in other.waypoints:
             other_scats.add(point.site_number)
 
-        return self_scats == other_scats
+        if self_scats == other_scats:
+            return True
+
+        # check if we have just gone through the same path
+        # eg. different directions but same path
+
+        def get_sequence_of_turns(directions: list[RouteDirection]) -> list[str]:
+            """Get a sequence of turns from a list of RouteDirections."""
+            no_straights = [
+                direction for direction in directions if not direction.is_straight
+            ]
+
+            return [direction.get_direction() for direction in no_straights]
+
+        self_turns = get_sequence_of_turns(self.directions)
+        other_turns = get_sequence_of_turns(other.directions)
+
+        return self_turns == other_turns
